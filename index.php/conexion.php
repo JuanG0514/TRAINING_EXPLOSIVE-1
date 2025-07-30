ffb3944869b7de2a2017a8b8cfa45244eb559889
JuanG <juandavidgome336@gmail.com>
2025-07-30 11:31:15 -0500

    se modifica la conexion.php

--- index.php/conexion.php
+++ index.php/conexion.php
@@ -1,12 +1,11 @@
 <?php
-function conexion(){
-  $servidor = 'containers-us-west-123.railway.app';  // cambia esto por el host que te da Railway
-  $usuario = 'root';
-  $contraseña = 'LuisaHerrera';
-  $bd = 'training_explosive'; // o el nombre real de tu base de datos
-  $puerto = 1234;  // reemplázalo por el puerto correcto
+function conexion() {
+  $servidor = 'mysql.railway.internal'; 
+  $usuario = 'root';                    
+  $contraseña = 'qSrqCfQScgSiMoMroHxKvimCxJNpIMAP'; 
+  $bd = 'railway';                     
+  $puerto = 3306;                      
 
-  // Crear conexión con puerto incluido
   $conexion = mysqli_connect($servidor, $usuario, $contraseña, $bd, $puerto);
 
   if (!$conexion) {
